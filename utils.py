490d570f82c7e91a9cabee68f357f6170224ef8d
CristianCtin <constantincristian@gmail.com>
2021-12-15 10:46:07 +0000

    Utilities finished

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 '''Task 0: Import the modules csv and random'''
-import csv.random
+import csv,random
 
 def startup():    
     """
@@ -37,8 +37,7 @@ def check_poke():
 
     :return: None if an invalid selection made otherwise an integer corresponding to a valid option
     """
-    print(
-        "Choose one of the options:\n1 = By Name\n2 = By Type")
+    print("Choose one of the options:\n1 = By Name\n2 = By Type")
     if opt in {1, 2}:
         return opt
 
@@ -76,6 +75,13 @@ def by_name(p_list = []):
     :param p_list: A list of pokemon
     :return: does not return anything
     """
+    p_name = input("Enter the name of a pokemon: ")
+    for pokemon in p_list:
+        if pokemon[1] == p_name:
+            print(pokemon)
+            return
+    print("Your pokemon does not exist on the list!")
+
 
 def by_type(p_list = []):
     
@@ -91,6 +97,13 @@ def by_type(p_list = []):
     :param p_list: A list of pokemon
     :return: does not return anything
     """
+    p_type = input("Enter the type of a pokemon: ")
+    for pokemon in p_list:
+        if pokemon[2] == p_type or pokemon[3] == p_type:
+            print(pokemon)
+            return
+    print("Your pokemon does not exist on the list!")
+
 
 def add_specific_poke():
     
@@ -104,6 +117,14 @@ def add_specific_poke():
     :param: None
     :return: A list representing a pokemon or None
     """
+    with open("pokemon_database.csv") as database:
+        reader = csv.reader(database)
+        p_name = input("Enter name of a pokemon:")
+        for pokemon in reader:
+            if pokemon[1] == p_name:
+                return pokemon
+        print("Pokemon not found!")
+
 
 def add_random_poke():
     
@@ -117,6 +138,16 @@ def add_random_poke():
     :return: A list representing a pokemon
     """
 
+    with open("pokemon_database.csv") as database:
+        reader = csv.reader(database)
+        number = random.randint(1, 801)
+        poke_list = []
+        for pokemon in reader:
+            poke_list.append(pokemon)
+        return poke_list[number]
+
+
+
 def show_all(pokedex = []):
     """
     Task 10: Print all pokemon from pokedex.
@@ -128,6 +159,11 @@ def show_all(pokedex = []):
     :return: None
     """
 
+    for pokemon in pokedex:
+        print(f"Name: {pokemon[1]}\nType: {pokemon[2]} {pokemon[3]}\n\nTotal: {pokemon[4]}\nHP: {pokemon[5]}\nGeneration {pokemon[11]}")
+
+
+
 def save_pokes(pokedex = []):
     """
     Task 11: Save content of the pokedex to a suitable file format
@@ -137,9 +173,23 @@ def save_pokes(pokedex = []):
     :return: None
     """
 
+    with open("pokedex.csv", "w") as pdex:
+        writer = csv.writer(pdex)
+        for pokemon in pokedex:
+            writer.writerow(pokemon)
+        print("Saving complete")
+
+
 def load_pokes(path):
     """
     Task 11.5: Load up pokemon from a CSV file into a list structure
     :param path: A relative file path to CSV file in string format
     :return: A list of pokemon
     """
+
+    p_list = []
+    with open (path) as f:
+        reader = csv.reader(f)
+        for pokemon in reader:
+            p_list.append(pokemon)
+    return p_list
